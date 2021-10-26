b39c565fe697b64b49df76035e1addf54935fae6
ZouPoYing <zoupoying@qq.com>
2021-10-26 10:49:34 +0800

    No9

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -2,10 +2,11 @@ package com.leetcode;
 
 import com.leetcode.exercises.no1.No1;
 import com.leetcode.exercises.no7.No7;
+import com.leetcode.exercises.no9.No9;
 
 public class Main {
 
     public static void main(String[] args) {
-        No7.f(-2147483412);
+        No9.f(214787412);
     }
 }
